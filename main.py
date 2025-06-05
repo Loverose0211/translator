f4b93472e7f66358bd2eecf293680e8b3654504f
Loverose0211 <xtong601@gmail.com>
2025-06-05 15:37:50 +0800

    进一步优化代码结构

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ from googletrans import Translator
 
 translator = Translator()
 
-src_lang = input("请输入原始语言代码（如 en=英文, zh-CN=中文, ja=日文）：")
-dest_lang = input("请输入目标语言代码（如 en=英文, zh-CN=中文, ja=日文）：")
+try:
+    src_lang = input("请输入原始语言代码（如 en=英文, zh-CN=中文, ja=日文）：")
+    dest_lang = input("请输入目标语言代码（如 en=英文, zh-CN=中文, ja=日文）：")
+    text = input("请输入要翻译的内容：")
 
-text = input("请输入要翻译的内容：")
+    result = translator.translate(text, src=src_lang, dest=dest_lang)
+    print("翻译结果：", result.text)
 
-result = translator.translate(text, src=src_lang, dest=dest_lang)
+except ValueError as ve:
+    print("❗语言代码无效,请检查拼写。示例:en,zh-CN,ja")
+    print("错误信息：", ve)
 
-print("翻译结果：", result.pronunciation)
+except Exception as e:
+    print("❗发生了未知错误，请重试")
+    print("错误信息：", e)
+
 
