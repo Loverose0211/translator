e918de1376f194e6d0061afff7898d01cc46871d
Loverose0211 <xtong601@gmail.com>
2025-06-05 14:57:55 +0800

    接入google翻译API

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-from translate import Translator
+from googletrans import Translator
 
-# 显示提示，让用户输入源语言和目标语言
-from_lang = input("请输入原始语言代码（如 en 表示英文）：")
-to_lang = input("请输入目标语言代码（如 zh 表示中文,ja 表示日文,fr 表示法文）：")
+translator = Translator()
 
-# 创建翻译器对象
-translator = Translator(from_lang=from_lang, to_lang=to_lang)
+src_lang = input("请输入原始语言代码（如 en=英文, zh-CN=中文, ja=日文）：")
+dest_lang = input("请输入目标语言代码（如 en=英文, zh-CN=中文, ja=日文）：")
 
-# 让用户输入要翻译的内容
 text = input("请输入要翻译的内容：")
 
-# 翻译并输出结果
-try:
-    translation = translator.translate(text)
-    print("翻译结果：", translation)
-except Exception as e:
-    print("翻译失败：", e)
+result = translator.translate(text, src=src_lang, dest=dest_lang)
+
+print("翻译结果：", result.pronunciation)
+
